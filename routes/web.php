ceefe4270a61f30e2ac3b04ea753d766023ce5ad
Faizan Abid <Faizanabid36@gmail.com>
2020-05-28 16:14:00 +0500

    sign in screen

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/faizan', function () {
     return view('welcome');
 });
+
+Route::get('/sign-in',function(){
+    return view('home.sign-in');
+})->name('sign-in');
