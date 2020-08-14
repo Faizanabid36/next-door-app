03e461c9370cf3410170e3383fbd0d9f824f9ae4
Faizan Abid <Faizanabid36@gmail.com>
2020-08-14 13:32:39 +0500

    changes

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,114 +18,113 @@ class UserSeeder extends Seeder
                 'name' => 'admin',
                 'email' => 'admin@mail.com',
                 'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 1,
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
                 'admin' => 1,
                 'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
             ],
-            [
-                'name' => 'Zain',
-                'email' => 'zain@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 1,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-26.jpg')
-            ],
-            [
-                'name' => 'Faizan',
-                'email' => 'faizanabid36@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'usaid',
-                'email' => 'usaid@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'mohisn',
-                'email' => 'mohsin@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'hamza',
-                'email' => 'hamza@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'zain khan',
-                'email' => 'zainkhan@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'Faizanabid',
-                'email' => 'faizanabid@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'Agent',
-                'email' => 'agent@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'is_public_agent'=>1,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ],
-            [
-                'name' => 'riffat',
-                'email' => 'riffat@gmail.com',
-                'password' => \Hash::make('1234567890'),
-                'address' => 'asdf-gh-021',
-                'contact' => '12312312',
-                'postal' => '123',
-                'gender' => 0,
-                'admin' => 0,
-                'is_public_agent'=>1,
-                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
-            ]
+//            [
+//                'name' => 'Zain',
+//                'email' => 'zain@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 1,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-26.jpg')
+//            ],
+//            [
+//                'name' => 'Faizan',
+//                'email' => 'faizanabid36@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'usaid',
+//                'email' => 'usaid@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'mohisn',
+//                'email' => 'mohsin@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'hamza',
+//                'email' => 'hamza@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'zain khan',
+//                'email' => 'zainkhan@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'Faizanabid',
+//                'email' => 'faizanabid@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'Agent',
+//                'email' => 'agent@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'is_public_agent'=>1,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ],
+//            [
+//                'name' => 'riffat',
+//                'email' => 'riffat@gmail.com',
+//                'password' => \Hash::make('1234567890'),
+//                'address' => 'asdf-gh-021',
+//                'contact' => '12312312',
+//                'postal' => '123',
+//                'gender' => 0,
+//                'admin' => 0,
+//                'is_public_agent'=>1,
+//                'avatar' => ('theme\app-assets\images\portrait\small\avatar-s-25.jpg')
+//            ]
         ];
         foreach ($user as $u)
             User::create($u);
