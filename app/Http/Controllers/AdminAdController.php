506f0247f31ccf559cae8b44e1806ebbbed86c60
Faizan Abid <Faizanabid36@gmail.com>
2020-09-04 21:26:41 +0500

    Admin Ads

---
+++ app/Http/Controllers/AdminAdController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AdminAd;
+use Illuminate\Http\Request;
+
+class AdminAdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AdminAd  $adminAd
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdminAd $adminAd)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AdminAd  $adminAd
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdminAd $adminAd)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AdminAd  $adminAd
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdminAd $adminAd)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AdminAd  $adminAd
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdminAd $adminAd)
+    {
+        //
+    }
+}
