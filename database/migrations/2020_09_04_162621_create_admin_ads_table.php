ed546133083d32f6e2c9333d17677e68ccae0fa5
Faizan Abid <Faizanabid36@gmail.com>
2020-09-04 21:42:48 +0500

    Admin Ad DB

--- database/migrations/2020_09_04_162621_create_admin_ads_table.php
+++ database/migrations/2020_09_04_162621_create_admin_ads_table.php
@@ -15,6 +15,12 @@ class CreateAdminAdsTable extends Migration
     {
         Schema::create('admin_ads', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('ad_heading');
+            $table->integer('ad_text');
+            $table->integer('ad_media');
+            $table->integer('visible_to_neighbourhood');
+            $table->integer('hide_after')->default(0);
             $table->timestamps();
         });
     }
