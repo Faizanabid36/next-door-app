986fddd9f90f5c4e4a8b4339d163aa7452fb511d
Faizan Abid <Faizanabid36@gmail.com>
2020-09-27 19:40:01 +0500

    Event UI

--- resources/views/web/frontend/events/single_event.blade.php
+++ resources/views/web/frontend/events/single_event.blade.php
@@ -25,6 +25,7 @@
         <div class="uk-card-default rounded mt-lg-4" uk-grid>
             <div class="uk-width-2-3@m mt-3 mb-3">
                 <h2 class="text-dark"><i>Description</i></h2>
+                <hr>
                 <h5>
                     {{$event->event_description}}
                 </h5>
@@ -55,29 +56,27 @@
         </div>
 
         <div class="uk-card-default rounded mt-lg-4" uk-grid>
-            <div class="uk-width-2-3@m mt-3 mb-3">
+            <div class="uk-width-3-3@m mt-3 mb-3">
                 <h2 class="text-dark"><i>People who showed interest</i></h2>
+                <hr>
                 @foreach($usersGoing  as $user)
-                    <h1>
-                        {{$user->name}}
-                    </h1>
+                    <div uk-grid>
+                        <div class="uk-width-2-3@m user-details-card pt-0 ml-4 mb-2">
+                            <div class="user-details-card-avatar" style="max-width: 50px">
+                                <img src="{{$user->avatar}}">
+                            </div>
+                            <div class="user-details-card-name">
+                                <h4 class="mb-0">
+                                    <a class="text-dark" href="{{route('view_profile',$user->id)}}">{{$user->name}}</a>
+                                </h4>
+                                <span class="text-dark"> {{$user->address}} </span>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3@m">
+                            <button class=" text-right button primary small rounded">Message </button>
+                        </div>
+                    </div>
                 @endforeach
-            </div>
-            <div class="uk-width-1-3@m mt-3 mb-3">
-                <div>
-                    <h5 class="mb-1">Posted By:</h5>
-                </div>
-                <div class="user-details-card pt-0">
-                    <div class="user-details-card-avatar" style="max-width: 50px">
-                        <img src="{{$event->creator->avatar}}" alt="">
-                    </div>
-                    <div class="user-details-card-name">
-                        <h4 class="mb-0">
-                            <a class="text-dark" href="{{route('view_profile',$event->creator->id)}}">{{$event->creator->name}}</a>
-                        </h4>
-                        <span> {{$event->created_at->diffForHumans()}} </span>
-                    </div>
-                </div>
             </div>
         </div>
 
