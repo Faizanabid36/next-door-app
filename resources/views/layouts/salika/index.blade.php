4388715a30604f993ebf6c5fdbfda7bb19a6c27a
Faizan Abid <Faizanabid36@gmail.com>
2020-10-06 23:53:35 +0500

    public agencies

--- resources/views/layouts/salika/index.blade.php
+++ resources/views/layouts/salika/index.blade.php
@@ -63,9 +63,7 @@
 
 <script src="{{asset('salika/assets/js/jquery-3.5.1.slim.min.js')}}"></script>
 <script src="{{asset('salika/assets/js/popper.min.js')}}"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
+<script src="{{asset('salika/assets/js/bootstrap.min.js')}}"></script>
 
 <script src="{{asset('salika/assets/js/framework.js?version='.time())}}"></script>
 <script src="{{asset('salika/assets/js/jquery-3.3.1.min.js?version='.time())}}"></script>
